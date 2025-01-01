e34a02f12f0b647fccf7ae031e002815cfeb7d3a
JHPrk <qkrwogus1236@gmail.com>
2025-01-02 01:36:19 +0900

    init first until domain

---
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/valueobject/StreetAddress.java
@@ -0,0 +1,9 @@
+package com.food.ordering.system.order.service.domain.valueobject;
+
+import java.util.UUID;
+
+public class StreetAddress {
+    private final UUID id;
+    private final String street;
+    private final String postalCode;
+}
