c3a6667baa26914b6655ff2f6fc6d2a6ff06a4ce
JHPrk <qkrwogus1236@gmail.com>
2025-01-17 01:28:58 +0900

    Implemented Customer, Restaurant 데이터 매퍼와 JPA Repository

---
+++ food-ordering-system/order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataacess/customer/entity/CustomerEntity.java
@@ -0,0 +1,23 @@
+package com.food.ordering.system.order.service.dataacess.customer.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "order_customer_m_view", schema = "customer")
+@Entity
+public class CustomerEntity {
+
+  @Id
+  private UUID id;
+
+}
