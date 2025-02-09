b8bca86f1e6a6827150d932f8af45743a916fe4e
JHPrk <qkrwogus1236@gmail.com>
2025-02-09 13:09:21 +0900

    Payment Service Kafka 모듈 구현하기

--- food-ordering-system/infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/KafkaMessageHelper.java
+++ food-ordering-system/infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/KafkaMessageHelper.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.order.service.messaging.publisher.kafka;
+package com.food.ordering.system.kafka.producer;
 
 import java.util.function.BiConsumer;
 import lombok.extern.slf4j.Slf4j;
@@ -8,14 +8,14 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class OrderKafkaMessageHelper {
+public class KafkaMessageHelper {
 
   public <T> BiConsumer<SendResult<String, T>, Throwable> getKafkaCallback(
-      String responseTopicName, T requestAvroModel, String orderId, String requestAvroModelName) {
+      String responseTopicName, T avroModel, String orderId, String avroModelName) {
     return (result, ex) -> {
       if (ex != null) {
         log.error("Error while sending {} message {} to topic {}",
-            requestAvroModelName, requestAvroModel.toString(), responseTopicName, ex);
+            avroModelName, avroModel.toString(), responseTopicName, ex);
         return;
       }
       RecordMetadata metadata = result.getRecordMetadata();
--- food-ordering-system/order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/publisher/kafka/CancelOrderKafkaMessagePublisher.java
+++ food-ordering-system/order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/publisher/kafka/CancelOrderKafkaMessagePublisher.java
@@ -1,6 +1,7 @@
 package com.food.ordering.system.order.service.messaging.publisher.kafka;
 
 import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.kafka.producer.KafkaMessageHelper;
 import com.food.ordering.system.kafka.producer.service.KafkaProducer;
 import com.food.ordering.system.order.service.domain.config.OrderServiceConfigData;
 import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
@@ -19,7 +20,7 @@ public class CancelOrderKafkaMessagePublisher implements
   private final OrderMessagingDataMapper orderMessagingDataMapper;
   private final OrderServiceConfigData orderServiceConfigData;
   private final KafkaProducer<String, PaymentRequestAvroModel> kafkaProducer;
-  private final OrderKafkaMessageHelper orderKafkaMessageHelper;
+  private final KafkaMessageHelper orderKafkaMessageHelper;
 
   @Override
   public void publish(OrderCancelledEvent domainEvent) {
