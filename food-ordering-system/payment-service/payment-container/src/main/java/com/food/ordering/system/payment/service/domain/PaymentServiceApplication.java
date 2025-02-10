cb30f9526a21b9b1abcf37b4dad29ad4d46fa788
JHPrk <qkrwogus1236@gmail.com>
2025-02-10 23:08:38 +0900

    payment container 설정파일 및 스키마 컨테이너 구현

--- food-ordering-system/payment-service/payment-container/src/main/java/com/food/ordering/system/payment/service/domain/PaymentServiceApplication.java
+++ food-ordering-system/payment-service/payment-container/src/main/java/com/food/ordering/system/payment/service/domain/PaymentServiceApplication.java
@@ -1,16 +1,16 @@
-package com.food.ordering.system.order.service.domain;
+package com.food.ordering.system.payment.service.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.food.ordering.system.order.service.dataaccess")
-@EntityScan(basePackages = "com.food.ordering.system.order.service.dataaccess")
+@EnableJpaRepositories(basePackages = "com.food.ordering.system.payment.service.dataaccess")
+@EntityScan(basePackages = "com.food.ordering.system.payment.service.dataaccess")
 @SpringBootApplication(scanBasePackages = "com.food.ordering.system")
-public class OrderServiceApplication {
+public class PaymentServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(OrderServiceApplication.class, args);
+    SpringApplication.run(PaymentServiceApplication.class, args);
   }
 }
