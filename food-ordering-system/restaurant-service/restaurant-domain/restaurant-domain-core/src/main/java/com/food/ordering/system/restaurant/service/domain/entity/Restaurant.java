7158c55e7452b2563fb3372a9d39e6f41eb2874f
JHPrk <qkrwogus1236@gmail.com>
2025-02-11 23:52:42 +0900

    Restaurant Entity 코어 구현

---
+++ food-ordering-system/restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/entity/Restaurant.java
@@ -0,0 +1,107 @@
+package com.food.ordering.system.restaurant.service.domain.entity;
+
+import com.food.ordering.system.domain.entity.AggregateRoot;
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.OrderApprovalStatus;
+import com.food.ordering.system.domain.valueobject.OrderStatus;
+import com.food.ordering.system.domain.valueobject.RestaurantId;
+import com.food.ordering.system.restaurant.service.domain.valueobject.OrderApprovalId;
+import java.util.List;
+import java.util.UUID;
+
+public class Restaurant extends AggregateRoot<RestaurantId> {
+
+  private OrderApproval orderApproval;
+  private boolean active;
+  private final OrderDetail orderDetail;
+
+  public void validateOrder(List<String> failureMessages) {
+    if (orderDetail.getOrderStatus() != OrderStatus.PAID) {
+      failureMessages.add(
+          String.format("Payment is not completed for order: {0}", orderDetail.getId()));
+    }
+    Money totalAmount = orderDetail.getProducts().stream().map(product -> {
+      if (product.isAvailable()) {
+        failureMessages.add(
+            String.format("Product with id: {0} is not available", product.getId().getValue()));
+      }
+      return product.getPrice().multiply(product.getQuantity());
+    }).reduce(Money.ZERO, Money::add);
+
+    if (!totalAmount.equals(orderDetail.getTotalAmount())) {
+      failureMessages.add(
+          String.format("Price total is not correct for order: {0}", orderDetail.getId()));
+    }
+  }
+
+  public void constructOrderApproval(OrderApprovalStatus orderApprovalStatus) {
+    this.orderApproval = OrderApproval.builder()
+        .orderApprovalId(new OrderApprovalId(UUID.randomUUID()))
+        .restaurantId(this.getId())
+        .orderId(this.getOrderDetail().getId())
+        .orderApprovalStatus(orderApprovalStatus)
+        .build();
+  }
+
+  public void setActive(boolean active) {
+    this.active = active;
+  }
+
+  private Restaurant(Builder builder) {
+    setId(builder.restaurantId);
+    orderApproval = builder.orderApproval;
+    active = builder.active;
+    orderDetail = builder.orderDetail;
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public OrderApproval getOrderApproval() {
+    return orderApproval;
+  }
+
+  public boolean isActive() {
+    return active;
+  }
+
+  public OrderDetail getOrderDetail() {
+    return orderDetail;
+  }
+
+  public static final class Builder {
+
+    private RestaurantId restaurantId;
+    private OrderApproval orderApproval;
+    private boolean active;
+    private OrderDetail orderDetail;
+
+    private Builder() {
+    }
+
+    public Builder id(RestaurantId val) {
+      restaurantId = val;
+      return this;
+    }
+
+    public Builder orderApproval(OrderApproval val) {
+      orderApproval = val;
+      return this;
+    }
+
+    public Builder active(boolean val) {
+      active = val;
+      return this;
+    }
+
+    public Builder orderDetail(OrderDetail val) {
+      orderDetail = val;
+      return this;
+    }
+
+    public Restaurant build() {
+      return new Restaurant(this);
+    }
+  }
+}
