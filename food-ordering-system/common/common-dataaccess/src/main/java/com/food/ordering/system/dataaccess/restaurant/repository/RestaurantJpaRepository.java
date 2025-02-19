00dd52ef78d582579ddfe53b39333b36f9cd52f3
JHPrk <qkrwogus1236@gmail.com>
2025-02-19 18:49:47 +0900

    Restaurant Data Access 모듈 구현, 일부 리팩토링

--- food-ordering-system/common/common-dataaccess/src/main/java/com/food/ordering/system/dataaccess/restaurant/repository/RestaurantJpaRepository.java
+++ food-ordering-system/common/common-dataaccess/src/main/java/com/food/ordering/system/dataaccess/restaurant/repository/RestaurantJpaRepository.java
@@ -1,6 +1,6 @@
-package com.food.ordering.system.order.service.dataaccess.restaurant.repository;
+package com.food.ordering.system.dataaccess.restaurant.repository;
 
-import com.food.ordering.system.order.service.dataaccess.restaurant.entity.RestaurantEntity;
+import com.food.ordering.system.dataaccess.restaurant.entity.RestaurantEntity;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
