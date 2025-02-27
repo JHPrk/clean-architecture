3edcd84c605bd715fde0fa7bb1e44c50d4090d5c
JHPrk <qkrwogus1236@gmail.com>
2025-02-27 11:16:55 +0900

    주문 도메인 결제 outbox 전송 scheduler 구현

---
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/outbox/scheduler/payment/PaymentOutboxHelper.java
@@ -0,0 +1,57 @@
+package com.food.ordering.system.order.service.domain.outbox.scheduler.payment;
+
+import static com.food.ordering.system.saga.order.SagaConstants.ORDER_SAGA_NAME;
+
+import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
+import com.food.ordering.system.order.service.domain.outbox.model.payment.OrderPaymentOutboxMessage;
+import com.food.ordering.system.order.service.domain.ports.output.repository.PaymentOutboxRepository;
+import com.food.ordering.system.outbox.OutboxStatus;
+import com.food.ordering.system.saga.SagaStatus;
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Component
+public class PaymentOutboxHelper {
+
+  private final PaymentOutboxRepository paymentOutboxRepository;
+
+  public PaymentOutboxHelper(PaymentOutboxRepository paymentOutboxRepository) {
+    this.paymentOutboxRepository = paymentOutboxRepository;
+  }
+
+  @Transactional(readOnly = true)
+  public Optional<List<OrderPaymentOutboxMessage>> getPaymentOutboxMessageByOutboxStatusAndSagaStatus(
+      OutboxStatus outboxStatus,
+      SagaStatus... sagaStatus) {
+    return paymentOutboxRepository.findByTypeAndOutboxStatusAndSagaStatus(
+        ORDER_SAGA_NAME, outboxStatus, sagaStatus);
+  }
+
+  @Transactional(readOnly = true)
+  public Optional<OrderPaymentOutboxMessage> getPaymentOutboxMessageBySagaIdAndSagaStatus(
+      UUID sagaId, SagaStatus... sagaStatus) {
+    return paymentOutboxRepository.findByTypeAndSagaIdAndSagaStatus(ORDER_SAGA_NAME, sagaId,
+        sagaStatus);
+  }
+
+  @Transactional
+  public void save(OrderPaymentOutboxMessage orderPaymentOutboxMessage) {
+    OrderPaymentOutboxMessage response = paymentOutboxRepository.save(orderPaymentOutboxMessage);
+    if (Objects.isNull(response)) {
+      log.error("Could not save OrderPaymentOutboxMessage with order id: {}",
+          orderPaymentOutboxMessage.getId());
+      throw new OrderDomainException(
+          MessageFormat.format("Could not save OrderPaymentOutboxMessage with order id: {0}",
+              orderPaymentOutboxMessage.getId()));
+    }
+    log.info("OrderPaymentOutboxMessage saved with outbox id: {}",
+        orderPaymentOutboxMessage.getId());
+  }
+}
